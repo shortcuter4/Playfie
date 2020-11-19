946afb97837c2b086cd85e9e97a756e10f5c0253
Subhan Ibrahimli <subhan.ibrahimli@hotmail.com>
2020-11-19 20:39:05 +0300

    views of the given urls

--- mysite/playfie/views.py
+++ mysite/playfie/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# view functions below
+
+# @shortcuter4
+# creating a homepage view here that is going to handle the traffic
+# from the home page of the news 
+
+def index(request):
+	return HttpResponse('<h1>News Home</h1>')
+
+def politics(request):
+	return HttpResponse('<h1>News Politics</h1>')
+
+def culture(request):
+	return HttpResponse('<h1>News Culture</h1>')
+
+def sport(request):
+	return HttpResponse('<h1>News Sport</h1>')
+
+def tourism(request):
+	return HttpResponse('<h1>News Tourism</h1>')
+
+def economy(request):
+	return HttpResponse('<h1>News Economy</h1>')
+
+'''
+def index(request):
+	return render(request, 'news/index.html')
+'''
