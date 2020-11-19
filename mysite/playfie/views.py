46dc4b98441c5db95f9bf285987d8d30cb4779df
Subhan Ibrahimli <subhan.ibrahimli@hotmail.com>
2020-11-19 21:01:33 +0300

    views updated

--- mysite/playfie/views.py
+++ mysite/playfie/views.py
@@ -9,22 +9,7 @@ from django.http import HttpResponse
 # from the home page of the news 
 
 def index(request):
-	return HttpResponse('<h1>News Home</h1>')
-
-def politics(request):
-	return HttpResponse('<h1>News Politics</h1>')
-
-def culture(request):
-	return HttpResponse('<h1>News Culture</h1>')
-
-def sport(request):
-	return HttpResponse('<h1>News Sport</h1>')
-
-def tourism(request):
-	return HttpResponse('<h1>News Tourism</h1>')
-
-def economy(request):
-	return HttpResponse('<h1>News Economy</h1>')
+	return HttpResponse('<h1> Home</h1>')
 
 '''
 def index(request):
