106f43cd1aa43e8d88735e671bf87bc987d30f6f
Subhan Ibrahimli <subhan.ibrahimli@hotmail.com>
2020-11-20 04:36:55 +0300

    url updated

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('playfie.urls')), # website homepage
+    path('playfie/', include('playfie.urls'))
 ]
